c14868eadba1142f1186be1baa5450c7740dff9c
lucasjanta <lucastyle2014@gmail.com>
2024-02-02 15:27:07 -0300

    O prazo vem chegando

--- Scenes/shoot_bar.gd
+++ Scenes/shoot_bar.gd
@@ -1,12 +1,16 @@
-extends RigidBody2D
+extends Node2D
+
+@onready var cursor = $CursorSprite
+
 # Called when the node enters the scene tree for the first time.
-var distancia : float
-var zombie_speed : float
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.x += zombie_speed
+	
 	pass
+
+func cursor_movement(mira):
+	cursor.position.x = mira
--- Scenes/world.gd
+++ Scenes/world.gd
@@ -5,10 +5,10 @@ extends Node2D
 var newZombie = preload("res://Scenes/enemy_1.tscn")
 var zombie_instance = null
 var zombies_in_screen = []
-var zombie_speed : float = 3
+#var zombie_speed : float = 3
 
-const PLAYER_START_POS := Vector2i(576, 432)
-const CAM_START_POS := Vector2i(576, 324)
+const PLAYER_START_POS := Vector2i(832, 376)
+const CAM_START_POS := Vector2i(832, 324)
 
 var speed : float
 var speed_multiplier : float
@@ -23,6 +23,12 @@ var anim_exec = false
 
 var player = null
 var enemy = null
+var distance_enemy_player
+
+var shoot_bar = preload("res://Scenes/shoot_bar.tscn")
+var shoot_bar_instance = null
+var mira : float =  0.0
+var mira_mult = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -31,6 +37,7 @@ func _ready() -> void:
 	$HUD/Keys_Colision.connect("erro", errou) #sinal do erro
 	screen_size = get_window().size
 	speed = INITIAL_SPEED
+	#foreground_handle()
 	new_game()
 
 func new_game():
@@ -39,8 +46,8 @@ func new_game():
 	game_running = false
 	$Player.position = PLAYER_START_POS
 	$Player.velocity = Vector2i(0, 0)
-	$Camera2D.position = CAM_START_POS
-	$TileMap.position = Vector2i(0, 0)
+	$Camera2D.position = CAM_START_POS 
+	#$TileMap.position = Vector2i(0, 0)
 	create_zombie()
 	$HUD.get_node("SpaceToPlayLabel").show()
 	
@@ -52,8 +59,9 @@ func _process(_delta):
 		score += speed
 		show_score()
 		player.position.x += speed
-		zombie_instance.position.x += zombie_speed
-		$Camera2D.position.x = (player.position.x + zombie_instance.position.x)/2
+		#if zombie_instance != null:
+			#zombie_instance.position.x += zombie_speed
+		$Camera2D.position.x = player.position.x
 		if speed > 0.4:
 			speed-= 0.01
 		if(speed > 2 and not anim_exec):
@@ -64,37 +72,89 @@ func _process(_delta):
 			sprite_2d.animation = "idle"
 			sprite_2d.stop()
 			anim_exec = false
-		if $Camera2D.position.x - $TileMap.position.x > screen_size.x * 1.5:
-			print("agora") #MUDAR ISSO AQUI ❤
-		#if $TileMap.global_position.x - zombie_instance.global_position.x > screen_size.x * 1.5:
-			#var offset = Vector2(screen_size.x, 0)
-			#$TileMap.global_position += offset
-	# Mova os inimigos na direção oposta ao movimento do TileMap para compensar
+		find_distance(zombies_in_screen)
+		if player.position.x > PLAYER_START_POS.x:
+			if Input.is_action_pressed("space"):
+				shoot_bar_instance.cursor_movement(player_shooting()) 
+			if Input.is_action_just_released("space"):
+				print("resultado: ", mira)
+				kill_zombies(find_closest_enemy_pos(zombies_in_screen))
+				mira = -150
+				mira_mult = 1
+				shoot_bar_instance.queue_free()
+				sprite_2d.flip_h = false
+				#shoot_bar_instance = null
+			
 			
 	else: 
 		if Input.is_action_just_pressed("space"):
 			game_running = true
 			$HUD.get_node("SpaceToPlayLabel").hide()
+	
 			
-	#if $Player.position.x - $Enemy1.position.x > 500:
-			#$Camera2D.position.x -= 50
+	
 func show_score():
 	$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score)
 	
 func acertou():
 	if speed < max_speed:
 		speed += 1.5
-	print("Player Pos: ", player.position.x, "Enemy Pos:", zombie_instance.position.x)
-	print("TileMap Pos: ", $TileMap.global_position.x)
+	print(find_closest_enemy_pos(zombies_in_screen))
+	#print("TileMap Pos: ", $TileMap.global_position.x)
 func errou():
 	if speed > 3:
 		speed -= 3.0
 
 func create_zombie():
 	zombie_instance = newZombie.instantiate()
-	#zombie_instance.position = ()
+	zombie_instance.zombie_speed = randf_range(2,8)
+	zombie_instance.position = Vector2i(460,400)
 	#zombie_speed = randf_range(speed, speed + 3)
 	add_child(zombie_instance)
 	zombies_in_screen.append(zombie_instance)
 	return zombie_instance
 	
+func find_closest_enemy_pos(zombies):
+	var closest
+	for i in range(zombies.size()):
+		distance_enemy_player = zombies[i].position.distance_to(player.position)
+		if closest == null:
+			closest = zombies[i]
+		elif distance_enemy_player < closest:
+			closest = zombies[i]
+		else:
+			return
+	return closest
+	
+func find_distance(array_zombies):
+	for zombie_enemy in array_zombies:
+		distance_enemy_player = zombie_enemy.position.distance_to(player.position)
+		zombie_enemy.distancia = distance_enemy_player
+
+func player_shooting():
+	speed = 0
+	sprite_2d.flip_h = true
+	if shoot_bar_instance == null:
+		shoot_bar_instance = shoot_bar.instantiate()
+		add_child(shoot_bar_instance)
+		shoot_bar_instance.position = player.position - Vector2(0,100)
+	
+	
+	if mira_mult == 1:
+		mira+= mira_mult * 2.5
+		if mira == 150:
+			mira_mult = -1
+	if mira_mult == -1:
+		mira+= mira_mult * 2.5
+		if mira == -150:
+			mira_mult = 1
+	return mira
+	
+func kill_zombies(closest_zombie):
+	if mira >= -15 and mira <= 15:
+		closest_zombie.queue_free()
+	
+#func new_zombies_man(player_pos):
+	#
+	#if player_pos = 
+	#create_zombie()
